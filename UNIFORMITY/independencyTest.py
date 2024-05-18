76765475ace20c3581a687486bdd4dde4176b9f7
AngelAldaz <angel.d.aldaz@gmail.com>
2024-05-18 04:14:13 -0600

    Previo a formateo

--- UNIFORMITY/independencyTest.py
+++ UNIFORMITY/independencyTest.py
@@ -1,15 +1,16 @@
 import multiplicativeCongruentMethod as multCM
 import os
+import pandas as pd
 
-def pokerizacion(muestra: list[int],limit: int) -> list[str]:
-  return [format(int(element * 100000), '05d') for element in muestra[:limit]]
+def pokerizacion(muestra: list[int]) -> list[str]:
+  return [format(int(element * 100000), '05d') for element in muestra]
   
 def todos_diferentes(numero):
-    return len(set(numero)) == 5
+  return len(set(numero)) == 5
 
 def contar_pares(numero):
-    cuenta_pares = sum(numero.count(digito) // 2 for digito in set(numero))
-    return cuenta_pares
+  cuenta_pares = sum(numero.count(digito) // 2 for digito in set(numero))
+  return cuenta_pares
 
 def tercia(numero):
   for digito in set(numero):
@@ -18,20 +19,20 @@ def tercia(numero):
   return False
 
 def full(numero):
-    # Obtener la cuenta de ocurrencias de cada dígito en el número
-    digit_counts = {digito: numero.count(digito) for digito in set(numero)}
-    
-    # Verificar si hay exactamente una tercia y exactamente un par
-    tercia_present = False
-    par_present = False
+  # Obtener la cuenta de ocurrencias de cada dígito en el número
+  digit_counts = {digito: numero.count(digito) for digito in set(numero)}
+  
+  # Verificar si hay exactamente una tercia y exactamente un par
+  tercia_present = False
+  par_present = False
 
-    for count in digit_counts.values():
-        if count == 3:
-            tercia_present = True
-        elif count == 2:
-            par_present = True
-    
-    return tercia_present and par_present
+  for count in digit_counts.values():
+    if count == 3:
+      tercia_present = True
+    elif count == 2:
+      par_present = True
+  
+  return tercia_present and par_present
 
 def poker(numero):
   for digito in set(numero):
@@ -46,44 +47,112 @@ def quintilla(numero):
   return False
 
 def contar_jugadas_poker(muestra):
-  listDiferentes = []
-  listUnPar = []
-  listDosPares = []
-  listTercia = []
-  listFull = []
-  listPoker = []
-  listQuintilla = []
+  resultados = {
+        'Todos diferentes': [],
+        'Un par': [],
+        'Dos pares': [],
+        'Una tercia': [],
+        'Full': [],
+        'Poker': [],
+        'Quintilla': []
+    }
   
   for numero in muestra:  
     if quintilla(numero):
-      listQuintilla.append(numero)
+      resultados['Quintilla'].append(numero)
       #print(f"QUINTILLA: {numero}")
     
     elif poker(numero):
-      listPoker.append(numero)
+      resultados['Poker'].append(numero)
       #print(f"POKER: {numero}")
     
     elif full(numero):
-      listFull.append(numero)
+      resultados['Full'].append(numero)
       #print(f"FULL: {numero}")
     
     elif tercia(numero):
-      listTercia.append(numero)
+      resultados['Una tercia'].append(numero)
       #print(f"TERCIA: {numero}")
     
     elif contar_pares(numero) == 2:
-      listDosPares.append(numero)
+      resultados['Dos pares'].append(numero)
       #print(f"2 PARES: {numero}")
     
     elif contar_pares(numero) == 1:
-      listUnPar.append(numero)
+      resultados['Un par'].append(numero)
       #print(f"1 PAR: {numero}")
     
     elif todos_diferentes(numero):
-      listDiferentes.append(numero)
+      resultados['Todos diferentes'].append(numero)
       #print(f"TODOS DIFERENTES: {numero}")
   
-  return listDiferentes, listUnPar, listDosPares, listTercia, listFull, listPoker, listQuintilla
+  return resultados
+
+def dataStructure(cantLongitudes:int, resultados:dict, totalMuestra:int):
+    data = {
+    'Clase (i)': [],
+    'Probabilidad Teorica de (i) P(i)': [],
+    'Frecuencia esperada de (i) Fe(i)=P(i)*n': [],
+    'Frecuencia observada de (i) Fo(i)': [],
+    'X²(i)': [],
+    }
+    pTeorica = [0.3024,0.5040,0.1080,0.0720,0.0090,0.0045,0.0001]
+    for i in range(cantLongitudes):
+      data['Clase (i)'].append(list(resultados.keys())[i])
+      data['Probabilidad Teorica de (i) P(i)'].append(pTeorica[i])
+      data['Frecuencia esperada de (i) Fe(i)=P(i)*n'].append(pTeorica[i]*totalMuestra)
+      data['Frecuencia observada de (i) Fo(i)'].append(len(resultados[list(resultados.keys())[i]]))
+      data['X²(i)'].append(((data['Frecuencia esperada de (i) Fe(i)=P(i)*n'][i]-data['Frecuencia observada de (i) Fo(i)'][i])**2)/data['Frecuencia esperada de (i) Fe(i)=P(i)*n'][i])
+    
+    return data
+
+def outDF(data):
+  df = pd.DataFrame(data)
+  print(df.to_string(index=False))
+
+def test_sequence(FE,fo, cantFO):
+  
+  nueva_lista = fo[:]  # Copia de la lista original para no modificarla
+  nueva_lista_FE = FE[:]  # Copia de la lista original para no modificarla
+  cont = 0
+  for i in range(len(fo)):
+    if fo[i] < cantFO and fo[i] != 0:
+      cont += 1
+      # Si el número es menor que la longitud de la lista, sumarlo con su vecino más cercano
+      if i == 0:
+        # Si es el primer número, sumar con el siguiente
+        nueva_lista[i+1] += nueva_lista[i]
+        nueva_lista[i] = 0
+        nueva_lista_FE[i+1] += nueva_lista_FE[i]
+        nueva_lista_FE[i] = 0
+      elif i == cantFO - 1:
+        # Si es el último número, sumar con el anterior
+        nueva_lista[i-1] += nueva_lista[i]
+        nueva_lista[i] = 0
+        nueva_lista_FE[i-1] += nueva_lista_FE[i]
+        nueva_lista_FE[i] = 0
+      elif abs(i - (cantFO - 1)) < i:
+        # Si es más cercano al final, sumar con el siguiente
+        nueva_lista[i+1] += nueva_lista[i]
+        nueva_lista[i] = 0
+        nueva_lista_FE[i+1] += nueva_lista_FE[i]
+        nueva_lista_FE[i] = 0
+      else:
+        # Si es más cercano al inicio, sumar con el anterior
+        nueva_lista[i-1] += nueva_lista[i]
+        nueva_lista[i] = 0
+        nueva_lista_FE[i-1] += nueva_lista_FE[i]
+        nueva_lista_FE[i] = 0
+      
+  if cont != 0:
+    test_sequence(nueva_lista_FE,nueva_lista, len(nueva_lista))
+  
+  nueva_lista = [elemento for elemento in nueva_lista if elemento != 0]
+  nueva_lista_FE = [elemento for elemento in nueva_lista_FE if elemento != 0]
+  
+  
+  return nueva_lista_FE, nueva_lista
+
 
 
 def main() -> None:
@@ -95,60 +164,66 @@ def main() -> None:
   cont = multCM.modular_sequence(a,m,X0)[0]
   numAl = multCM.modular_sequence(a,m,X0)[-1]
   
-  listPokerizada = pokerizacion(numAl,cont)
+  numeros = [
+    .72422, .45444, .34633, .77676, .13173, .62783, .24529, .27489, 
+    .37149, .43923, .61394, .21569, .28490, .23439, .51505, .88888, 
+    .24177, .94949, .59333, .77399, .92992, .67879, .55452, .34347, .45850
+  ]
+  
+  #listPokerizada = pokerizacion(numAl[:cont])
+  listPokerizada = pokerizacion(numeros)
   
   #Resultados
+  resultados = contar_jugadas_poker(listPokerizada)
+  
+  data = dataStructure(len(resultados), resultados, len(listPokerizada))
+  outDF(data)
+  nueva_lista_FO, nueva_lista = test_sequence(data['Frecuencia esperada de (i) Fe(i)=P(i)*n'],data['Frecuencia observada de (i) Fo(i)'], len(data['Frecuencia observada de (i) Fo(i)']))
+  
+  print(nueva_lista_FO)
+  print(nueva_lista)
+  
+  #n = list(resultados.keys())
+  #print(list(resultados.keys()))
+  """
   print("-----RESULTADOS-----")
-  """
-  for i in range(59,70):
-    print(f"{i+1}: {contar_jugadas_poker(listPokerizada[i])}")
-  """
-  
-  """
-  numeros = [
-    '72422', '45444', '34633', '77676', '13173', '62783', '24529', '27489', 
-    '37149', '43923', '61394', '21569', '28490', '23439', '51505', '88888', 
-    '24177', '94949', '59333', '77399', '92992', '67879', '55452', '34347', '45850'
-  ]
-  """
-  
-  listDiferentes, listUnPar, listDosPares, listTercia, listFull, listPoker, listQuintilla = contar_jugadas_poker(listPokerizada)
-  
+    
   #Diferentes
   print("Diferentes")
-  print(f"Cantidad: {len(listDiferentes)}")
-  print(listDiferentes)
+  print(f"Cantidad: {len(resultados['Todos diferentes'])}")
+  print(resultados['Todos diferentes'])
   
   #UnPar
   print("UnPar")
-  print(f"Cantidad: {len(listUnPar)}")
-  print(listUnPar)
+  print(f"Cantidad: {len(resultados['Un par'])}")
+  print(resultados['Un par'])
   
   #DosPares
   print("DosPares")
-  print(f"Cantidad: {len(listDosPares)}")
-  print(listDosPares)
+  print(f"Cantidad: {len(resultados['Dos pares'])}")
+  print(resultados['Dos pares'])
   
   #Tercia
   print("Tercia")
-  print(f"Cantidad: {len(listTercia)}")
-  print(listTercia)
+  print(f"Cantidad: {len(resultados['Una tercia'])}")
+  print(resultados['Una tercia'])
   
   #Full
   print("Full")
-  print(f"Cantidad: {len(listFull)}")
-  print(listFull)
+  print(f"Cantidad: {len(resultados['Full'])}")
+  print(resultados['Full'])
   
   #Poker
   print("Poker")
-  print(f"Cantidad: {len(listPoker)}")
-  print(listPoker)
+  print(f"Cantidad: {len(resultados['Poker'])}")
+  print(resultados['Poker'])
   
   #Quintilla
   print("Quintilla")
-  print(f"Cantidad: {len(listQuintilla)}")
-  print(listQuintilla)
+  print(f"Cantidad: {len(resultados['Quintilla'])}")
+  print(resultados['Quintilla'])
   
+  """
 
 if __name__ == "__main__":
   main()
